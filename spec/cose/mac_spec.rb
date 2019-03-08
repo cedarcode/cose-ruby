2024bb2a58063df98975ae0ad664b58d70ac5080
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-08 00:00:06 -0300

    feat: support deserialize of a COSE_Mac

---
+++ spec/cose/mac_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "cbor"
+require "cose/mac"
+require "cose/recipient"
+
+RSpec.describe "COSE::Mac" do
+  context ".deserialize" do
+    before do
+      cbor = CBOR.encode(
+        [
+          CBOR.encode(1 => 2),
+          { 3 => 4 },
+          CBOR.encode("This is the content"),
+          "tag".b,
+          [
+            CBOR.encode([CBOR.encode(5 => 6), { 6 => 7 }, "ciphertextA".b]),
+            CBOR.encode([CBOR.encode(8 => 9), { 10 => 11 }, "ciphertextB".b])
+          ]
+        ]
+      )
+
+      @mac = COSE::Mac.deserialize(cbor)
+    end
+
+    it "returns protected headers" do
+      expect(@mac.protected_headers).to eq(1 => 2)
+    end
+
+    it "returns unprotected headers" do
+      expect(@mac.unprotected_headers).to eq(3 => 4)
+    end
+
+    it "returns the payload" do
+      expect(@mac.payload).to eq("This is the content")
+    end
+
+    it "returns the tag" do
+      expect(@mac.tag).to eq("tag")
+    end
+
+    it "returns the recipients" do
+      expect(@mac.recipients.size).to eq(2)
+      expect(@mac.recipients.all? { |s| s.is_a?(COSE::Recipient) }).to be_truthy
+    end
+  end
+end
