d6ee3941e09b15b83acc11b2144b7e43aabad41f
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-07 23:42:51 -0300

    feat: support deserialize of a COSE_Recipient

---
+++ lib/cose/recipient.rb
@@ -0,0 +1,24 @@
+require "cose/security_message"
+
+module COSE
+  class Recipient < SecurityMessage
+    attr_reader :ciphertext, :recipients
+
+    def self.keyword_arguments_for_initialize(decoded)
+      keyword_arguments = { ciphertext: decoded[0] }
+
+      if decoded[1]
+        keyword_arguments[:recipients] = decoded[1].map { |s| COSE::Recipient.deserialize(s) }
+      end
+
+      keyword_arguments
+    end
+
+    def initialize(ciphertext:, recipients: nil, **keyword_arguments)
+      super(**keyword_arguments)
+
+      @ciphertext = ciphertext
+      @recipients = recipients
+    end
+  end
+end
