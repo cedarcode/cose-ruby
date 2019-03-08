fa58225ebad95885f49841cd6d74756b6716ebed
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-07 23:50:09 -0300

    feat: support deserialize of a COSE_Encrypt

---
+++ lib/cose/encrypt.rb
@@ -0,0 +1,19 @@
+require "cose/security_message"
+require "cose/recipient"
+
+module COSE
+  class Encrypt < SecurityMessage
+    attr_reader :ciphertext, :recipients
+
+    def self.keyword_arguments_for_initialize(decoded)
+      { ciphertext: decoded[0], recipients: decoded[1].map { |s| COSE::Recipient.deserialize(s) } }
+    end
+
+    def initialize(ciphertext:, recipients:, **keyword_arguments)
+      super(**keyword_arguments)
+
+      @ciphertext = ciphertext
+      @recipients = recipients
+    end
+  end
+end
---
+++ spec/cose/encrypt_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "cbor"
+require "cose/encrypt"
+require "cose/recipient"
+
+RSpec.describe "COSE::Encrypt" do
+  context ".deserialize" do
+    before do
+      cbor = CBOR.encode(
+        [
+          CBOR.encode(1 => 2),
+          { 3 => 4 },
+          "ciphertext".b,
+          [
+            CBOR.encode([CBOR.encode(5 => 6), { 6 => 7 }, "ciphertextA".b]),
+            CBOR.encode([CBOR.encode(8 => 9), { 10 => 11 }, "ciphertextB".b])
+          ]
+        ]
+      )
+
+      @encrypt = COSE::Encrypt.deserialize(cbor)
+    end
+
+    it "returns protected headers" do
+      expect(@encrypt.protected_headers).to eq(1 => 2)
+    end
+
+    it "returns unprotected headers" do
+      expect(@encrypt.unprotected_headers).to eq(3 => 4)
+    end
+
+    it "returns the recipients" do
+      expect(@encrypt.recipients.size).to eq(2)
+      expect(@encrypt.recipients.all? { |s| s.is_a?(COSE::Recipient) }).to be_truthy
+    end
+  end
+end
