cbb300edffd6f63ebe4873f22146801581853ed6
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-08-25 19:51:11 -0300

    fix: follow RFC 8230 and use salt length as per the hash function

--- lib/cose/signature_verifier/pss.rb
+++ lib/cose/signature_verifier/pss.rb
@@ -11,7 +11,7 @@ module COSE
         pkey = key.to_pkey
 
         if pkey.respond_to?(:verify_pss)
-          pkey.verify_pss(hash_function, signature, verification_data, salt_length: :auto, mgf1_hash: hash_function)
+          pkey.verify_pss(hash_function, signature, verification_data, salt_length: :digest, mgf1_hash: hash_function)
         else
           raise(COSE::Error, "Update to openssl gem >= v2.1 to have PSS support")
         end
